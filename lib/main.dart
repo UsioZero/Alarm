a177f5114aadd194c30d3efeb8de003137c983fe
Oleg <swortaartonlineforever@gmail.com>
2020-06-16 16:57:33 +0300

    +Notify

--- lib/main.dart
+++ lib/main.dart
@@ -1,59 +1,20 @@
-import 'dart:ui';
-
 import 'package:flutter/material.dart';
-import 'package:overlay_support/overlay_support.dart';
+import 'Allarm.dart';
 
-void main() => runApp(Allarm());
+void main() => runApp(MyApp());
 
-class Allarm extends StatelessWidget {
+class MyApp extends StatelessWidget {
+
   @override
-  Widget build(BuildContext context) {
-    return OverlaySupport(
-      child: MaterialApp(
-          title: 'Clock',
-          theme: ThemeData(
-            primarySwatch: Colors.yellow,
-            textTheme: TextTheme(bodyText1: TextStyle(color: Colors.white))
-          ),
-          home: Scaffold(
-            appBar: AppBar(
-              title: Text('Clock'),
-              leading: Container(
-               child: Icon(Icons.alarm, color: Colors.white70),
-              padding: EdgeInsets.only(right: 5),               
-              ),
-            ),
-            floatingActionButton: FloatingActionButton(
-              onPressed: (){
-              showOverlayNotification((context) {
-                  return Card(
-                    margin: const EdgeInsets.symmetric(horizontal: 4),
-                    child: SafeArea(
-                      child: ListTile(
-                        leading: SizedBox.fromSize(
-                          size: const Size(40, 40),
-                          child: ClipOval(
-                            child: Container(
-                              color: Colors.yellow,
-                            )
-                          )
-                        ),
-                        title: Text('Timer is ready'),
-                        trailing: IconButton(
-                          icon: Icon(Icons.close),
-                          onPressed: () {
-                             OverlaySupportEntry.of(context).dismiss();
-                          }
-                        ),
-                      ),
-                    ),
-                  );
-                });
-              },
-              child: Icon(Icons.add_alarm),
-            )
-          )
-      )
-    );
-  }
+  Widget build(BuildContext context){
+    return MaterialApp(
+        title: 'Clock',
+        theme: ThemeData(
+          primarySwatch: Colors.yellow,
+          textTheme: TextTheme(bodyText1: TextStyle(color: Colors.white))
+        ),
+      home: Allarm(),
+    );  }
+
 }
+
