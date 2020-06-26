0256eb970f49f721a760d3741abbb961f73ca4cf
Oleg <swortaartonlineforever@gmail.com>
2020-06-27 01:05:39 +0300

    second version of animated container

--- lib/screens/alarms/alarm_list.dart
+++ lib/screens/alarms/alarm_list.dart
@@ -17,12 +17,11 @@ class AlarmListState extends State<AlarmList> {
 
   DateTime selectedTime;
 
-
   Future _waitTimeFuture(BuildContext context) async {
     setState(() {
       simpleNotifications.add(new SimpleNotification(
-        _onAndroidSelectNotification, _onIOSSelectNotification));
-    }); 
+          _onAndroidSelectNotification, _onIOSSelectNotification));
+    });
   }
   // Notification handling
 
@@ -69,54 +68,111 @@ class AlarmListState extends State<AlarmList> {
         ),
       ),
       body: Container(
-          child: ListView.builder(
-              itemCount: simpleNotifications.length,
-              itemBuilder: (context, i) {
-                return Card(
-                  elevation: 2,
-                  margin: EdgeInsets.symmetric(horizontal: 4, vertical: 2),
-                  child: Container(
-                    decoration: BoxDecoration(color: Colors.yellow.shade300),
-                    child: ListTile(
-                      contentPadding: EdgeInsets.symmetric(horizontal:10),
+        child: ListView.builder(
+          itemCount: simpleNotifications.length,
+          itemBuilder: (context, i) {
+            return Card(
+                elevation: 2,
+                margin: EdgeInsets.symmetric(horizontal: 4, vertical: 2),
+                child: Container(
+                  decoration: BoxDecoration(color: Colors.yellow.shade300),
+                  child: Column(children: <Widget>[
+                    ListTile(
+                      contentPadding: EdgeInsets.symmetric(horizontal: 10),
                       leading: Container(
-                        child: Icon(Icons.alarm, color: Colors.black,),
+                        child: Icon(
+                          Icons.alarm,
+                          color: Colors.black,
+                        ),
                         padding: EdgeInsets.only(right: 12),
                         decoration: BoxDecoration(
-                          border: Border(
-                            right: BorderSide(
-                              width: 1,
-                              color: Colors.black
-                            )
-                          )
-                        ),
+                            border: Border(
+                                right:
+                                    BorderSide(width: 1, color: Colors.black))),
                       ),
                       title: Text(
-                          DateFormat('hh:mm').format(simpleNotifications[i].dateTime),
-                         style: TextStyle(
-                           fontWeight: FontWeight.bold,
-                           fontSize: 20,
-                          ),
+                        DateFormat('hh:mm')
+                            .format(simpleNotifications[i].dateTime),
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 20,
+                        ),
                       ),
-                      subtitle: Text(
-                        DateFormat('dd-MM-yyyy').format(simpleNotifications[i].dateTime)
-                      ),
-                      trailing: Icon(Icons.keyboard_arrow_down, color: Colors.black),
+                      subtitle: Text(DateFormat('dd-MM-yyyy')
+                          .format(simpleNotifications[i].dateTime)),
+                      trailing:
+                          Icon(Icons.keyboard_arrow_down, color: Colors.black),
                     ),
-                  ),
-                );
-              },
-            ),
+                    // AnimatedContainer(
+                    //   duration: Duration(milliseconds: 500),
+                    //   curve: Curves.fastOutSlowIn,
+                    //   height: simpleNotifications[i].cardHeight,
+                    //   child: Card(
+                    //     child: Column(
+                    //       children: <Widget>[
+                    //         SwitchListTile(
+                    //           value: simpleNotifications[i].isShowNotify,
+                    //           onChanged: (bool value) {
+                    //             setState(() {
+                    //               simpleNotifications[i].isShowNotify = value;
+                    //             });
+                    //           },
+                    //         ),
+                    //         DaySelector(
+                    //           onChange: (value) {},
+                    //           mode: DaySelector.modeFull,
+                    //         ),
+                    //         Row(
+                    //           crossAxisAlignment: CrossAxisAlignment.center,
+                    //           mainAxisAlignment: MainAxisAlignment.center,
+                    //           verticalDirection: VerticalDirection.down,
+                    //           children: <Widget>[
+                    //             FloatingActionButton(
+                    //               heroTag: "floattag21321313332",
+                    //               backgroundColor: Colors.red,
+                    //               foregroundColor: Colors.white,
+                    //               child: Icon(Icons.delete),
+                    //               onPressed: () {
+                    //                 setState(() {
+                    //                   simpleNotifications.removeAt(i);
+                    //                 });
+                    //               },
+                    //             ),
+                    //             RaisedButton(
+                    //               splashColor: Theme.of(context).primaryColor,
+                    //               highlightColor:
+                    //                   Theme.of(context).primaryColor,
+                    //               color: Colors.yellow,
+                    //               onPressed: () {
+                    //                 _buttonPressed(i);
+                    //               },
+                    //               child: Text(
+                    //                 'Set time',
+                    //                 style: TextStyle(
+                    //                   fontWeight: FontWeight.bold,
+                    //                   fontSize: 20,
+                    //                 ),
+                    //               ),
+                    //             ),
+                    //           ],
+                    //         )
+                    //       ],
+                    //     ),
+                    //   ),
+                    // )
+                  ]),
+                ));
+          },
+        ),
       ),
       floatingActionButton: FloatingActionButton(
-        backgroundColor: Colors.blueGrey,
-        foregroundColor: Colors.white,
-        //shape: ShapeBorder,
-        child: Icon(Icons.add),
-        onPressed: (){
-          _waitTimeFuture(context);
-        }
-      ),
+          backgroundColor: Colors.blueGrey,
+          foregroundColor: Colors.white,
+          //shape: ShapeBorder,
+          child: Icon(Icons.add),
+          onPressed: () {
+            _waitTimeFuture(context);
+          }),
       bottomNavigationBar: BottomAppBar(
         color: Colors.yellow,
         child: Container(height: 50.0),
